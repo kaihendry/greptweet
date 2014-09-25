ab8aa55eddd47e652188dbffeb253df79dcfb7b9
Kai Hendry <hendry@webconverger.com>
2014-09-25 14:29:30 +0800

    Search for fixed strings (i.e. disallow regexp)

--- grep.php
+++ grep.php
@@ -10,7 +10,7 @@ $QUERY=escapeshellarg($_GET['q']);
 //fwrite($fp, $_GET['q'] . " : " . $QUERY . "\n");
 //fclose($fp);
 
-exec("zgrep -hi $QUERY tweets.txt", $array);
+exec("zgrep -Fhi $QUERY tweets.txt", $array);
 $data = json_encode($array);
 echo $_GET['jsoncallback'] . '(' . $data . ');';
 ?>
